34c2677414138263e70e6fafbc537d68c2ee8483
lucascool12 <71119084+lucascool12@users.noreply.github.com>
2022-11-15 07:54:07 +0100

    Betere ding ofzo

--- PakketManager-war/src/java/Servlets/GenericHandler.java
+++ PakketManager-war/src/java/Servlets/GenericHandler.java
@@ -9,6 +9,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -37,16 +40,23 @@ public class GenericHandler extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try{
             URI uri = new URI(request.getRequestURI());
-            String path = uri.getPath();
-            String lastPart = path.substring(path.lastIndexOf('/') + 1);
-            if(lastPart.equals("Koerier")){
-                request.setAttribute("path", lastPart);
+            ArrayList<String> path = new ArrayList<String>(Arrays.asList(uri.getPath().split("/")));
+            path.remove(0); //empty entry because URI path startswith /
+            System.out.println(path);
+            //if moet van eerste entries naar laatste omdat java suckt
+            if(path.get(0).equals("PakketManager-war") && path.size() == 1){
+                request.setAttribute("path", path.get(0));
+                RequestDispatcher rd = request.getRequestDispatcher("test.jsp");
+                rd.forward(request, response);
+            }else if(path.get(1).equals("Koerier")){
+                request.setAttribute("path", path.get(1));
                 RequestDispatcher rd = request.getRequestDispatcher("test.jsp");
                 rd.forward(request, response);
             }else{
                 response.sendError(404);
             }
-        }catch(URISyntaxException e){
+        }catch(URISyntaxException | IndexOutOfBoundsException e){
+            System.out.println(e);
             throw new ServletException("Bad request");
         }
     }
